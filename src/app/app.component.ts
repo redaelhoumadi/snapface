94ce4c6a17186e1997d9317621cbdab908254d33
redaelhoumadi <reda.elhoumadi@gmail.com>
2024-11-08 11:53:46 +0100

    add component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [FaceSnapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
