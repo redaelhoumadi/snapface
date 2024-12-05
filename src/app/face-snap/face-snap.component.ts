f7414608ff43a38cee822cf1c545abb7412ec4fc
redaelhoumadi <reda.elhoumadi@gmail.com>
2024-12-05 10:34:20 +0100

    add img, button, data

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,6 +7,38 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent  implements OnInit {
+title!: string;
+description!: string;
+createdAt!: Date;
+snaps!: number;
+imageUrl!: string;
+snapButtonText!: string;
+userHasSnapped!: boolean;
 
+ngOnInit(): void {
+  this.title ='Archibald';
+  this.description = 'Mon meilleure ami depuis toujours !  ';
+  this.createdAt = new Date();
+  this.snaps = 0;
+  this.imageUrl = 'https://cdn.futura-sciences.com/sources/images/AI-creation.jpg';
+  this.snapButtonText = 'Oh Snap !!';
 }
+onSnaps(): void {
+  if (this.userHasSnapped){
+    this.unSnap();
+  } else {
+    this.snap();
+  }}
+  unSnap(){
+    this.snaps--;
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
+  }
+  snap(){
+    this.snaps++;
+    this.snapButtonText = 'oopd unsnap!';
+    this.userHasSnapped = true;
+  }
+}
+
